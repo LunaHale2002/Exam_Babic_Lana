c8e3af1c3a3c4dea4c2ab44c0df1722c85752a9d
LunaHale2002 <lbabic@edenschool.fr>
2024-01-26 15:48:34 +0100

    exercices

---
+++ main.js
@@ -0,0 +1,164 @@
+// ## Exercice 1.
+// Écrivez une fonction qui prend un parametre, qui vérifie si ce parametre est un nombre qui retourne vrai si 
+// c'est un nombre et faux dans le cas contraire. Vous devez utiliser [isNaN()](https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/isNaN) au sein de cette fonction.
+
+// ## Exercice 2.
+// Écrivez une fonction qui prend un parametre qui vérifie si ce parametre est un booleen qui retourne vrai si 
+// c'est un booleen et faux dans le cas contraire. Vous devez utiliser `typeof` au sein de cette fonction.
+
+// ## Exercice 3.
+// Corriger la fonction suivante pour qu'elle retourne false.
+
+function checkEqual(a,b){
+    return a = b;
+
+}
+checkEqual(5,"5");
+console.log();
+
+
+// ## Exercice 4.
+// Corriger la fonction suivante pour qu'elle retourne true
+
+function checkAge() {
+    return a > 10;
+}
+checkAge(12);
+console.log();
+
+
+// ## Exercice 5
+// Completez la fonction suivante pour qu'elle retourne false. Vous devez utilisez un opérateur de négation
+
+function isNotAdmin(a) {
+    return a != 10
+}
+isNotAdmin(true);
+console.log();
+
+// ## Exercice 6
+// Écrivez une fonction JavaScript appelée `sommeChiffres` qui prend un tablau de nombres entier en entrée et 
+// retourne la somme de ses chiffres. Par exemple, si l'entrée est `[1, 2, 3]`, la fonction devrait retourner 6 
+// (car 1 + 2 + 3 = 6).
+// Vous devez utiliser des boucles `for` et ne pas utiliser de méthodes telles que `reduce`.
+
+function sommeChiffres(T) {
+    let = [1,2,3];
+    for(let i=0; i == T.length; i++){
+        console.log();
+    }
+}
+sommeTableau([1, 2, 3]);
+
+
+// ## Exercice 7 
+// Écrivez une fonction JavaScript appelée `plusGrand` qui prend un tableau de nombres en entrée et 
+// retourne le plus grand nombre du tableau.
+// Vous devez utiliser des boucles `for` et ne pas utiliser de méthodes telles que `reduce`.
+
+function plusGrand(T) {
+    let = [];
+    for(let i=0; i > T.length; i++){
+        console.log();
+    }
+    return 
+}
+plusGrand([3, 2, 6, 1, 4]);
+
+// ## Exercice 8
+// Écrivez une fonction JavaScript appelée `trouverPlusLongMot` qui prend une chaîne de caractères (une phrase) en entrée
+// et retourne le mot le plus long dans la phrase. Un mot est défini comme une série de caractères séparés 
+// par des espaces.
+// Vous devez utiliser des boucles `for`.
+
+function trouverPlusLongMot() {
+    for(let i=0; i ;){
+        console.log()
+    }
+}
+compterMots("Ceci est une super phrase de test");
+
+// ## Exercice 9
+// Écrivez une fonction JavaScript appelée triCroissant qui prend un tableau de nombres en entrée et 
+// retourne le tableau trié par ordre croissant.
+// Vous devez utiliser des boucles `for`.
+
+function triCroissant(T) {
+    let = [];
+    for(let i=0; i < T.length; i++){
+        console.log();
+    }
+    return
+}
+triCroissant(["3, 2, 5, 6, 1"]);
+
+// ## Exercice 10
+// Écrivez une fonction JavaScript appelée `inverserTableau` qui prend un tableau en entrée et 
+// retourne un nouveau tableau contenant les éléments du tableau d'origine dans l'ordre inverse. 
+// Par exemple, si le tableau d'entrée est [1, 2, 3], la fonction devrait retourner [3, 2, 1].
+// Vous devez utiliser des boucles `for`.
+
+function inverserTableau(T) {
+    let = [];
+    for(let i=0; i ;){
+        console.log()
+    }
+}
+inverserTableau([1, 2, 3]);
+
+// ## Exercice 11
+// Écrivez une fonction JavaScript appelée `aDesDoublons` qui prend un tableau de mots en entrée et 
+// retourne `true` si le tableau contient des éléments en double (doublons), sinon retourne `false`.
+// Vous devez utiliser des boucles `for`.
+
+function aDesDoublons(T) {
+    let = [];
+    for (let i = 0; i < T.length; i++) {
+        console.log();
+    }
+    return
+}
+aDesDoublons(["mot", "stylo", "phrase", "mot", "jogging"]);
+aDesDoublons(["mot", "stylo", "phrase", "jogging"]);
+
+// ## Exercice 12
+// Écrivez une fonction JavaScript appelée `produitTableau` qui prend un tableau de nombres en entrée et 
+// retourne le produit de tous les éléments du tableau.
+// Vous devez utiliser des boucles `for`.
+
+function produitTableau(T) {
+    let = [];
+    for(let i=0; i ;){
+        console.log();
+    }
+    return
+}
+produitTableau([2, 3, 6]);
+
+// ## Exercice 13
+// Écrivez une fonction JavaScript appelée `rechercherElement` qui prend un tableau et un élément en entrée, 
+// puis retourne true si l'élément est présent dans le tableau, sinon retourne false.
+// Vous devez utiliser des boucles `for`.
+
+function rechercherElement(T) {
+    let = [];
+    for(let i=0; i ;){
+        console.log();
+    }
+    return
+}
+rechercherElement(["chaise", "table", "vase"], "table");
+rechercherElement(["chaise", "table", "vase"], "plateau");
+
+// ## Exercice 14
+// Écrivez une fonction JavaScript appelée `fusionnerTableaux` qui prend deux tableaux en entrée et 
+// retourne un nouveau tableau qui est la fusion des deux tableaux d'origine.
+// Vous devez utiliser des boucles `for`.
+
+function fusionnerTableaux(T1, T2) {
+    let = [];
+    for(let i=0; i ;){
+        console.log();
+    }
+}
+fusionnerTableaux([2, 3, 4], [1, 2, 5]);
